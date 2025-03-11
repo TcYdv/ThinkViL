1aba8f49ded36d22b1b050d61d65eee5596d7d6f
Sunil Kumar <sunilkumarbakoliya123@gmail.com>
2025-03-12 01:24:24 +0530

    payment integration with backend

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -19,3 +19,52 @@ export const getToken = () => {
 export const logout = () => {
     localStorage.removeItem("token");
   };
+
+
+
+/**
+ * Creates a payment order
+ * @param {string} amount - Payment amount
+ * @returns {Promise<object>} - API response
+ */
+export const createPaymentOrder = async (amount: string): Promise<object> => {
+  try {
+    const response = await axiosClient.post("/api/payment/create-order", { amount });
+    return response; // Return the API response
+  } catch (error) {
+    console.error("createPaymentOrder Error:", error);
+    throw error; // Rethrow error for handling in the calling function
+  }
+};
+
+/**
+ * Verifies payment after successful transaction
+ * @param {object} paymentData - Payment response from Razorpay
+ * @param {string} token - JWT token for authentication
+ * @returns {Promise<boolean>} - Verification success/failure
+ */
+export const verifyPayment = async (paymentData: Object): Promise<boolean> => {
+  try {
+    const response = await axiosClient.post("/api/payment/verify-payment", {paymentData});
+    return response.data;
+  } catch (error) {
+    console.error("varify Payment Error:", error);
+    throw error; // Rethrow error for handling in the calling function
+  }
+};
+
+/**
+ * Saves donation details after successful payment
+ * @param {object} donationData - User & donation details
+ * @param {string} token - JWT token for authentication
+ * @returns {Promise<boolean>} - Whether donation was saved successfully
+ */
+export const saveDonation = async (donationData: Object): Promise<boolean> => {
+  try {
+    const response = await axiosClient.post("/api/donation/save", {donationData});
+    return response.data;
+  } catch (error) {
+    console.error("save Payment Error:", error);
+    throw error; // Rethrow error for handling in the calling function
+  }
+};
